bdfa293a473f1074963e055b8935a0f5c88efa0a
Allan Tiotonho <allanpereira773@gmail.com>
2024-02-24 18:57:32 -0300

    Algorithm to convert image format created

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.IOException;
+
+import entities.ImageConverter;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String inputImage = "C:\\Users\\allan\\Downloads\\michael-martin-photo.jpg";
+		String outputImage = "C:\\Users\\allan\\Downloads\\michael-martin-photo.png";
+		String imageFormat = "PNG";
+		
+		try {
+			boolean result = ImageConverter.convertFormat(inputImage, outputImage, imageFormat);
+			
+			if(result) {
+				System.out.println("Image converted sucessfully!");
+			}else {
+				System.out.println("Error: fail to convert image!");
+			}
+		}catch(IOException e) {
+			System.out.println("Error during converting image");
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/entities/ImageConverter.java
@@ -0,0 +1,29 @@
+package entities;
+
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageConverter {
+
+	public static boolean convertFormat(String inputImagePath, String outputImagePath, String formatName)
+			throws IOException {
+		
+		FileInputStream inputStream = new FileInputStream(inputImagePath);
+		FileOutputStream outputStream = new FileOutputStream(outputImagePath);
+		
+		//lê a imagem input lá do arquivo
+		BufferedImage inputImage = ImageIO.read(inputStream);
+		
+		boolean result = ImageIO.write(inputImage, formatName, outputStream);
+		
+		outputStream.close();
+		inputStream.close();
+		
+		return result;
+	}
+
+}
