bb66c1c1cf882660640125ca555e1a60132829cc
Allan Tiotonho <allanpereira773@gmail.com>
2024-02-26 13:20:10 -0300

    Update: open image

--- src/entities/ImageConverterGUI.java
+++ src/entities/ImageConverterGUI.java
@@ -4,9 +4,13 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -21,10 +25,13 @@ public class ImageConverterGUI extends JPanel{
 	
 	private JTextField imagePath = new JTextField();
 	
-	private ImageIcon icon;
-	private JLabel imagelbl;
+	private JLabel imagelbl = new JLabel();
 	
-	private JScrollPane scrollPane;
+	private JScrollPane scrollPane = new JScrollPane();
+	
+	private JFileChooser chooseFile = new JFileChooser();
+	
+	private JPanel panel = new JPanel();
 
 	public ImageConverterGUI() {
 		run();
@@ -33,23 +40,24 @@ public class ImageConverterGUI extends JPanel{
 	public void run() {
 		layout = new GridBagLayout();
 		setLayout(layout);
+		panel.setLayout(layout);
 		
 		setPreferredSize(new Dimension(400, 400));
 		
-		icon = new ImageIcon("");
-		imagelbl = new JLabel();
-		scrollPane = new JScrollPane(imagelbl);
-		
-		GridBagConstraints scrollConst = new GridBagConstraints();
-		scrollConst.gridx = GridBagConstraints.RELATIVE;
-		scrollConst.gridy = GridBagConstraints.RELATIVE;
-		scrollConst.fill = GridBagConstraints.VERTICAL;
-		scrollConst.fill = GridBagConstraints.HORIZONTAL;
-		scrollConst.weighty = 2.0;
-		scrollConst.gridwidth = 3;
-		scrollConst.insets = new Insets(10, 10, 10, 10);
-		scrollPane.setPreferredSize(new Dimension(400, 470));
-		add(scrollPane, scrollConst);
+		openImage.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// execute action to open the image
+				chooseFile.showSaveDialog(null);
+				if(chooseFile.getSelectedFile() != null) {
+					String absolutePath = chooseFile.getSelectedFile().getAbsolutePath(); 
+					imagePath.setText(absolutePath);
+					imagelbl.setIcon(new ImageIcon(absolutePath));
+					panel.add(imagelbl);
+					scrollPane.setViewportView(panel);
+				}
+			}
+		});;
 		
 		GridBagConstraints gbc1 = new GridBagConstraints();
 		gbc1.gridx = 0;
@@ -65,6 +73,7 @@ public class ImageConverterGUI extends JPanel{
 		gbc2.weighty = 1.0;
 		gbc2.fill = GridBagConstraints.HORIZONTAL;
 		gbc2.insets = new Insets(0, 5, 5, 10);
+		imagePath.setEditable(false);
 		add(imagePath, gbc2);
 		
 		//Insets(int top, int left, int bottom, int right)
@@ -75,6 +84,17 @@ public class ImageConverterGUI extends JPanel{
 		gbc3.anchor = GridBagConstraints.WEST;
 		gbc3.insets = new Insets(5, 10, 30, 10);
 		add(convertClick, gbc3);
+		
+		GridBagConstraints scrollConst = new GridBagConstraints();
+		scrollConst.gridx = GridBagConstraints.RELATIVE;
+		scrollConst.gridy = GridBagConstraints.RELATIVE;
+		scrollConst.fill = GridBagConstraints.VERTICAL;
+		scrollConst.fill = GridBagConstraints.HORIZONTAL;
+		scrollConst.weighty = 2.0;
+		scrollConst.gridwidth = 3;
+		scrollConst.insets = new Insets(10, 10, 10, 10);
+		scrollPane.setPreferredSize(new Dimension(400, 470));
+		add(scrollPane, scrollConst);
 		
 	}
 
